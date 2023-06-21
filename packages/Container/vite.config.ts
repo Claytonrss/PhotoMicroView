d17d7d6b099750092b7fbf4b355cb0af4cb01646
Clayton Souza <clayton.rssouza@gmail.com>
2023-06-21 18:39:18 -0300

    chore: config module fedaration

--- packages/Container/vite.config.ts
+++ packages/Container/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "container",
+      filename: "remoteEntry.js",
+      remotes: {
+        PhotoList: "photolist@http://localhost:3001/remoteEntry.js",
+        PhotoDetail: "photodetail@http://localhost:3002/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
--- packages/PhotoList/vite.config.ts
+++ packages/PhotoList/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "photolist",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./PhotoList": "./src/PhotoList",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
